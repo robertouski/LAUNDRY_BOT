7e9e5daec5cc5df2c1f656d93a767606c753d265
robertouski <rmoncayoandrade@gmail.com>
2024-05-03 09:58:14 -0500

    Ready to ADD ID ASSISTANT with business info

---
+++ src/utils/layer/interpreterLayer.js
@@ -0,0 +1,27 @@
+const { interpreterResponse } = require("../ai/responseIA");
+
+module.exports = async(ctx, ctxFn)=>{
+  try{
+    const ai = await ctxFn.extensions.ai
+  const messages = ai.getHistory(ctx.from)
+  console.log(messages)
+  const IAinterpreter = await interpreterResponse(messages, ai)
+
+  console.log('AIinterpreter:', IAinterpreter)
+  if(IAinterpreter === 'INFORMACION'){
+    return await ctxFn.flowDynamic('Dejame te proporciono INFO')
+  }
+  if(IAinterpreter === 'AGENDAR'){
+    return await ctxFn.flowDynamic('Espera que aun no puedo AGENDAR')
+  }
+  if(IAinterpreter === 'AGENTE'){
+    return await ctxFn.flowDynamic('Espera que aun no puedo AGENTE')
+  }
+  if(IAinterpreter === 'JESSICA'){
+    return await ctxFn.flowDynamic('Espera que aun no puedo ponerte a Jessica')
+  }
+  }
+  catch(error){
+    console.log('Error Interpreter:', error)
+  }
+  }
